80d481e944f38a55f1346c15686c4f7b8d0ab9bb
eun-choi <54615424+eun-choi@users.noreply.github.com>
2021-02-18 12:06:29 +0900

    ver update

--- src/ios/KakaoOpenSDK.framework/Headers/KakaoOpenSDK.h
+++ src/ios/KakaoOpenSDK.framework/Headers/KakaoOpenSDK.h
@@ -28,4 +28,4 @@ FOUNDATION_EXPORT const unsigned char KakaoOpenSDKVersionString[];
 #import <KakaoOpenSDK/KOSessionTask+AgeAuthAPI.h>
 #import <KakaoOpenSDK/KOSessionTask+AppFriendAPI.h>
 
-#define KAKAO_SDK_IOS_VERSION_STRING    @"1.23.5"
+#define KAKAO_SDK_IOS_VERSION_STRING    @"1.23.7"
