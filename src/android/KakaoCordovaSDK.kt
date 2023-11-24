6073d3fbe2bffbab62785d09638e54d37e00c87f
eun-choi <54615424+eun-choi@users.noreply.github.com>
2023-11-25 00:59:27 +0900

    ios login fix

--- src/android/KakaoCordovaSDK.kt
+++ src/android/KakaoCordovaSDK.kt
@@ -8,12 +8,6 @@ import android.util.Log
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.model.AuthError
 import com.kakao.sdk.common.util.KakaoCustomTabsClient
-import com.kakao.sdk.share.ShareClient
-import com.kakao.sdk.share.WebSharerClient
-import com.kakao.sdk.template.model.Button
-import com.kakao.sdk.template.model.Content
-import com.kakao.sdk.template.model.FeedTemplate
-import com.kakao.sdk.template.model.Link
 import com.kakao.sdk.user.UserApiClient
 import org.apache.cordova.CallbackContext
 import org.apache.cordova.CordovaArgs
@@ -48,93 +42,14 @@ class KakaoCordovaSDK : CordovaPlugin() {
         }
       }
 
-      "sendLinkFeed" -> {
-        val arg = args?.get(0)
-
-        if (arg != null && arg is JSONObject && callbackContext != null) {
-          this.sendLinkFeed(arg, callbackContext)
+      "logout" -> {
+        if (callbackContext != null) {
+          this.logout(callbackContext)
         }
       }
     }
 
     return true
-  }
-
-  private fun sendLinkFeed(arg: JSONObject, callbackContext: CallbackContext) {
-    val template = FeedTemplate(content = this.createContent(arg.getJSONObject("content")), social = null, buttons = this.createButtons(arg.getJSONArray("buttons")))
-    val serverCallbackArgs = HashMap<String, String>()
-    val serverCallbackArgsJSON = arg.getJSONObject("content").getJSONObject("serverCallbackArgs")
-    val it = serverCallbackArgsJSON.keys()
-
-    while (it.hasNext()) {
-      val key = it.next()
-      val keyValue = serverCallbackArgsJSON.get(key)
-      serverCallbackArgs[key] = keyValue.toString()
-    }
-
-    if (ShareClient.instance.isKakaoTalkSharingAvailable(this.cordova.context)) {
-      // 앱 띄우고 공유
-      ShareClient.instance.shareDefault(this.cordova.context, template, serverCallbackArgs) { sharingResult, error ->
-        if (error != null) {
-          callbackContext.error(error.localizedMessage)
-          return@shareDefault
-        }
-
-        if (sharingResult != null) {
-          // 결과 버림
-          this.cordova.activity.startActivity(sharingResult.intent)
-          callbackContext.success()
-        } else {
-          callbackContext.error("Cannot read share info")
-        }
-      }
-    } else {
-      // 웹 띄우고 공유
-      val sharerUrl = WebSharerClient.instance.makeDefaultUrl(template, serverCallbackArgs)
-      try {
-        this.cordova.activity.let {
-          KakaoCustomTabsClient.openWithDefault(it, sharerUrl)
-        }
-      } catch (e: UnsupportedOperationException) {
-        try {
-          this.cordova.activity.let {
-            KakaoCustomTabsClient.open(it, sharerUrl)
-          }
-        } catch (e: ActivityNotFoundException) {
-          callbackContext.error("Cannot open share link")
-        }
-      }
-    }
-  }
-
-  private fun createButton(obj: JSONObject): Button {
-    val title: String = obj.getString("title")
-    val link: Link = createLink(obj.getJSONObject(("link")))
-    return Button(title, link)
-  }
-
-  private fun createButtons(obj: JSONArray): List<Button> {
-    val buttons = mutableListOf<Button>()
-
-    for (i in 0 until obj.length()) {
-      buttons.add(this.createButton(obj.getJSONObject(i)))
-    }
-
-    return buttons
-  }
-
-  private fun createContent(obj: JSONObject): Content {
-    val title: String = obj.getString("title")
-    val url: String = obj.getString("imageURL")
-    val link: Link = createLink(obj.getJSONObject("link"))
-    val desc: String? = obj.getString("desc")
-    return Content(title, description = desc, imageUrl = url, link = link)
-  }
-
-  private fun createLink(obj: JSONObject): Link {
-    val webURL: String? = obj.getString("webURL")
-    val mobileWebURL: String? = obj.getString("mobileWebURL")
-    return Link(webUrl = webURL, mobileWebUrl = mobileWebURL)
   }
 
   private fun login(callbackContext: CallbackContext) {
@@ -198,6 +113,18 @@ class KakaoCordovaSDK : CordovaPlugin() {
       } else {
         callbackContext.error("User not found")
         return@me
+      }
+    }
+  }
+
+  private fun logout(callbackContext: CallbackContext) {
+    UserApiClient.instance.logout() { error ->
+      if (error != null) {
+        callbackContext.error("Kakao Logout Error")
+        return@logout
+      } else {
+        callbackContext.success("Kakao Logout Success")
+        return@logout
       }
     }
   }
--- src/ios/KakaoCordovaSDK.swift
+++ src/ios/KakaoCordovaSDK.swift
@@ -1,27 +1,25 @@
 import Foundation
 import KakaoSDKAuth
 import KakaoSDKCommon
-import KakaoSDKShare
-import KakaoSDKTemplate
 import KakaoSDKUser
-import SafariServices
 
 class KakaoCordovaSDK: CDVPlugin {
-  var safariViewController: SFSafariViewController?
-  var rootViewController: UIViewController?
 
   override func pluginInitialize() {
     let key = Bundle.main.object(forInfoDictionaryKey: "KAKAO_APP_KEY") as? String
     if let key = key {
       KakaoSDK.initSDK(appKey: key)
+      print("KakaoSDK initSDK.")
+      // NotificationCenter.default.addObserver(self, selector: #selector(handleOpenURL(_:)), name: NSNotification.Name.CDVPluginHandleOpenURLWithAppSourceAndAnnotation, object: nil)
     }
   }
 
   override func handleOpenURL(_ notification: Notification!) {
+    print("KakaoSDK handleOpenURL.")
     if let _url = notification.object as? NSURL {
       if let _url = _url.absoluteString {
         if let url = URL(string: _url) {
-          if AuthApi.isKakaoTalkLoginUrl(url) {
+          if (AuthApi.isKakaoTalkLoginUrl(url)) {
             AuthController.handleOpenUrl(url: url)
           }
         }
@@ -31,38 +29,39 @@ class KakaoCordovaSDK: CDVPlugin {
 
   @objc(login:) func login(command: CDVInvokedUrlCommand) {
     DispatchQueue.main.async {
-      if UserApi.isKakaoTalkLoginAvailable() {
+      print("KakaoSDK login.")
+      /*if (UserApi.isKakaoTalkLoginAvailable()) {
         UserApi.shared.loginWithKakaoTalk(
-          completion: {
-            oauthToken, error in
+          launchMethod: .CustomScheme, completion: {
+            (oauthToken, error) in
             self.loginCallback(oauthToken: oauthToken, error: error, callbackId: command.callbackId)
           }
         )
-      } else {
+      } else {*/
         UserApi.shared.loginWithKakaoAccount(
           completion: {
-            oauthToken, error in
+            (oauthToken, error) in
             self.loginCallback(oauthToken: oauthToken, error: error, callbackId: command.callbackId)
           }
         )
-      }
     }
   }
 
   func loginCallback(oauthToken: OAuthToken?, error: Error?, callbackId: String) {
+    print("KakaoSDK loginCallback.")
     if error != nil {
       let result = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: error?.localizedDescription)
-      commandDelegate.send(result, callbackId: callbackId)
+      self.commandDelegate.send(result, callbackId: callbackId)
     } else if let oauthToken = oauthToken {
-      UserApi.shared.me {
-        user, error in
+      UserApi.shared.me() {
+        (user, error) in
         if error != nil {
           let result = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: error?.localizedDescription)
           self.commandDelegate.send(result, callbackId: callbackId)
         } else {
           let result = CDVPluginResult(
             status: CDVCommandStatus_OK,
-            messageAs: ["id": user!.id!, "result": true, "email": user!.kakaoAccount!.email!, "accessToken": oauthToken.accessToken]
+            messageAs: ["id": user?.id as Any, "result": true, "email": user?.kakaoAccount?.email as Any, "accessToken": oauthToken.accessToken, "profileImage": user?.kakaoAccount?.profile?.profileImageUrl!.absoluteString as Any]
           )
           self.commandDelegate.send(result, callbackId: callbackId)
         }
@@ -72,6 +71,7 @@ class KakaoCordovaSDK: CDVPlugin {
 
   @objc(logout:) func logout(command: CDVInvokedUrlCommand) {
     DispatchQueue.main.async {
+      print("KakaoSDK logout.")
       UserApi.shared.logout {
         (error) in
         if let error = error {
@@ -83,147 +83,5 @@ class KakaoCordovaSDK: CDVPlugin {
         }
       }
     }
-  }
-
-  @objc(sendLinkFeed:) func sendLinkFeed(command: CDVInvokedUrlCommand) {
-    if let dict = command.arguments.last as? NSDictionary {
-      let buttons = createButtons(dict: dict)
-      let buttonTitle = (dict["buttonTitle"] as? String)
-      let content = dict["content"] as! NSDictionary
-      var serverCallbackArgs: [String: String] = [:]
-      if let arg = content["serverCallbackArgs"] as? NSDictionary {
-        let keys = arg.allKeys.compactMap { $0 as? String }
-        for key in keys {
-          let keyValue = arg.value(forKey: key)
-          if let keyValue = keyValue as? [String: Any] {
-            let obj = try? JSONSerialization.data(withJSONObject: keyValue)
-            if let obj = obj {
-              serverCallbackArgs[key] = String(data: obj, encoding: .utf8)
-            }
-          } else {
-            serverCallbackArgs[key] = String(describing: keyValue!)
-          }
-        }
-      }
-
-      let feedTemplate = FeedTemplate(content: createContent(dict: content), social: createSocial(dict: dict), buttonTitle: buttonTitle, buttons: buttons)
-      if let feedTemplateJsonData = (try? SdkJSONEncoder.custom.encode(feedTemplate)) {
-        if let templateJsonObject = SdkUtils.toJsonObject(feedTemplateJsonData) {
-          shareDefaultTemplate(templateObject: templateJsonObject, serverCallbackArgs: serverCallbackArgs) { result, error in
-            if let error = error {
-              let result = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: error.localizedDescription)
-              self.commandDelegate.send(result, callbackId: command.callbackId)
-            } else {
-              let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: result)
-              self.commandDelegate.send(result, callbackId: command.callbackId)
-            }
-          }
-        }
-      }
-    }
-  }
-
-  private func openLinkWebview(url: URL, callback: @escaping (Bool, Error?) -> Void) {
-    DispatchQueue.main.async {
-      self.safariViewController = SFSafariViewController(url: url)
-      self.safariViewController?.modalPresentationStyle = .pageSheet
-
-      self.rootViewController = UIApplication.shared.delegate?.window??.rootViewController
-      self.rootViewController?.dismiss(animated: false, completion: {
-        let appDelegate = UIApplication.shared.delegate
-        appDelegate?.window??.rootViewController?.present(self.safariViewController!, animated: true, completion: { callback(true, nil) })
-      })
-    }
-  }
-
-  private func shareDefaultTemplate(templateObject: [String: Any], serverCallbackArgs: [String: String], callback: @escaping (Bool, Error?) -> Void) {
-    if ShareApi.isKakaoTalkSharingAvailable() == true {
-      ShareApi.shared.shareDefault(templateObject: templateObject, serverCallbackArgs: serverCallbackArgs) { linkResult, error in
-        if let error = error {
-          callback(false, error)
-        } else {
-          // do something
-          guard let linkResult = linkResult else { return }
-          UIApplication.shared.open(linkResult.url, options: [:], completionHandler: nil)
-          callback(true, nil)
-        }
-      }
-    } else {
-      if let url = ShareApi.shared.makeDefaultUrl(templateObject: templateObject) {
-        openLinkWebview(url: url, callback: callback)
-      }
-    }
-  }
-
-  private func createSocial(dict: NSDictionary) -> Social? {
-    if let socialDict = dict["social"] {
-      let sDict = socialDict as! NSDictionary
-      let commentCount = (sDict["commentCount"] as? Int)
-      let likeCount = (sDict["likeCount"] as? Int)
-      let sharedCount = (sDict["sharedCount"] as? Int)
-      let subscriberCount = (sDict["subscriberCount"] as? Int)
-      let viewCount = (sDict["viewCount"] as? Int)
-      return Social(likeCount: likeCount, commentCount: commentCount, sharedCount: sharedCount, viewCount: viewCount, subscriberCount: subscriberCount)
-    }
-
-    return nil
-  }
-
-  private func createContent(dict: NSDictionary) -> Content {
-    let title = dict["title"] != nil ? (dict["title"] as! String) : ""
-    let imageUrl = dict["imageURL"] != nil ? createURL(dict: dict, key: "imageURL")! : URL(string: "")!
-    let link = createLink(dict: dict, key: "link")
-    let description = (dict["desc"] as? String)
-    let imageWidth = (dict["imageWidth"] as? Int)
-    let imageHeight = (dict["imageHeight"] as? Int)
-    return Content(title: title, imageUrl: imageUrl, imageWidth: imageWidth, imageHeight: imageHeight, description: description, link: link)
-  }
-
-  private func createURL(dict: NSDictionary, key: String) -> URL? {
-    if let value = dict[key] {
-      return URL(string: value as! String)
-    }
-
-    return nil
-  }
-
-  private func createExecutionParams(dict: NSDictionary, key: String) -> [String: String]? {
-    if let dictArr = dict[key] {
-      var returnDict: [String: String] = [:]
-      for item in dictArr as! NSArray {
-        if let returnKey = (item as! NSDictionary)["key"], let returnValue = (item as! NSDictionary)["value"] {
-          returnDict[returnKey as! String] = (returnValue as! String)
-        }
-      }
-      return returnDict
-    }
-    return nil
-  }
-
-  private func createLink(dict: NSDictionary, key: String) -> Link {
-    if let linkDict = dict[key] {
-      let lDict = (linkDict as! NSDictionary)
-      let webUrl = createURL(dict: lDict, key: "webURL")
-      let mobileWebUrl = createURL(dict: lDict, key: "mobileWebURL")
-      return Link(webUrl: webUrl, mobileWebUrl: mobileWebUrl)
-    }
-    return Link(webUrl: nil, mobileWebUrl: nil, androidExecutionParams: nil, iosExecutionParams: nil)
-  }
-
-  private func createButton(dict: NSDictionary) -> Button {
-    let title = dict["title"] != nil ? dict["title"] : ""
-    let link = createLink(dict: dict, key: "link")
-    return Button(title: title as! String, link: link)
-  }
-
-  private func createButtons(dict: NSDictionary) -> [Button]? {
-    if let dictArr = dict["buttons"] {
-      var buttons: [Button] = []
-      for item in dictArr as! NSArray {
-        buttons.append(createButton(dict: item as! NSDictionary))
-      }
-      return buttons
-    }
-    return nil
   }
 }
